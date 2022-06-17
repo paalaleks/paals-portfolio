4f7151b389c971042d94cc2f13c5741d19d0569a
paalaleks <70602624+paalaleks@users.noreply.github.com>
2022-06-17 21:01:07 +0200

    commit again

--- scripts/api/baseUrl.js
+++ scripts/api/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = "https://paals-portfolio-blog.herokuapp.com/";
+export const baseUrl = "https://paals-portfolio-blog.herokuapp.com";
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,32 @@
+import { baseUrl } from "./api/baseUrl.js";
 import artwork from "./modal.js";
-import getArticles from "./api/getArticles.js";
+// import getArticles from "./api/getArticles.js";
 
 artwork();
 getArticles();
+
+const blog = document.querySelector(".blog-section");
+
+async function getArticles() {
+  const res = await fetch(baseUrl + "/" + "api/articles?populate=image");
+  const json = await res.json();
+  const data = json.data;
+
+  console.log(data);
+
+  data.forEach((element) => {
+    blog.innerHTML += `
+      <a class="blog" href="">
+        <div>
+          <img src="${baseUrl}${element.attributes.image.data[0].attributes.url}" alt="" />    
+        </div>
+        <h2>${element.attributes.title}</h2>
+        <p>
+        ${element.attributes.excerpt}
+        </p>
+      </a>     
+    `;
+  });
+}
+
+export default getArticles;
