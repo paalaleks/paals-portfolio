e90dd8df74568a4a0ef9212881c04fd0e7550226
paalaleks <70602624+paalaleks@users.noreply.github.com>
2022-06-21 09:24:37 +0200

    last update

--- scripts/artworkModal.js
+++ scripts/artworkModal.js
@@ -1,12 +1,15 @@
 const artworkLink = document.querySelector(".artwork-link");
 const intro = document.querySelector(".intro");
 const exitArtwork = document.querySelector(".exit-artwork");
+const roses = document.querySelector(".roses");
 
 export default function artwork() {
   artworkLink.addEventListener("click", () => {
     intro.classList.toggle("toggleIntro");
+    roses.style.display = "none";
   });
   exitArtwork.addEventListener("click", () => {
     intro.style.display = "block";
+    roses.style.display = "block";
   });
 }
